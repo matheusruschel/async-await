7959c9034459546b92674ccb5d1cff57afcb6650
Matheus Ruschel <mruschel@tribalscale.com>
2024-05-07 10:38:14 -0400

    First commit, Xcode setup

---
+++ MTG-Learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MTG-Learning
+//
+//  Created by Matheus Ruschel on 2024-05-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MTG-Learning/MTG_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_LearningApp.swift
+//  MTG-Learning
+//
+//  Created by Matheus Ruschel on 2024-05-07.
+//
+
+import SwiftUI
+
+@main
+struct MTG_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
